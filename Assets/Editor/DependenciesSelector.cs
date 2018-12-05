dffc7031f44d47ff5e8172de63abc59856ca7c44
AsanCai <969850420@qq.com>
2018-12-05 17:22:09 +0800

    添加svn的add和commit操作

--- Assets/Editor/DependenciesSelector.cs
+++ Assets/Editor/DependenciesSelector.cs
@@ -6,36 +6,37 @@ using System.Collections.Generic;
 namespace Dependency{
     public class DependenciesSelector : EditorWindow {
         // 上一个选中的prefab对象
-        static Object preObj;
+        private static Object m_preObj;
         // 当前选中的prefab对象
-        static Object obj;
+        private static Object m_obj;
         // obj的所有依赖对象
-        static List<Object> selectedObjs = new List<Object>();
-        // 保存存放资源的文件夹路径
-        static string folder;
-        static Vector2 scrollPos;
+        private static List<Object> m_selectedObjs = new List<Object>();
+
+        private static Vector2 m_scrollPos;
+        // private static bool m_addFolder;
+
         // 等待复制的文件个数
-        static int remainCount;
+        private static int m_remainCount;
 
         // 保存项目的绝对路径        
-        static string projectDir = System.Environment.CurrentDirectory.Replace('\\', '/');
-        // 保存所有依赖资源的路径
-        static List<string> dependentPaths = new List<string>();
+        private static string PROJECTDIR = System.Environment.CurrentDirectory.Replace('\\', '/');
         
+        // 保存所有依赖资源的路径
+        private static Dictionary<string, bool> m_originalPaths = new Dictionary<string, bool>();
+        // m_originalPaths的keys
+        private static List<string> m_dependentPaths = new List<string>();
 
         [MenuItem("Assets/Find Prefab Dependencies")]
-        static void FindPrefabDependencies(){
-            preObj = null;
-            obj = null;
-            folder = "";
+        private static void FindPrefabDependencies(){
+            m_preObj = null;
+            m_obj = null;
 
             if(Selection.activeObject == null){
                 return;
             }
 
             if(PrefabUtility.GetPrefabType(Selection.activeObject) != PrefabType.None) {
-                obj = (Selection.activeObject) as GameObject;
-                Debug.Log(obj.name);
+                m_obj = (Selection.activeObject) as GameObject;
             }
 
             Init();
@@ -44,89 +45,94 @@ namespace Dependency{
         private static void Init() {
             DependenciesSelector window = (DependenciesSelector)EditorWindow.GetWindow(typeof(DependenciesSelector), false, "Dependencies");
             window.Show();
+
+            window.minSize = new Vector2(480, 300);
+            window.maximized = false;
         }
 
-        void OnGUI() {
+        private void OnGUI() {
             EditorGUILayout.BeginVertical();
             
             EditorGUILayout.BeginHorizontal();
                 EditorGUILayout.LabelField("Prefab：", GUILayout.Width(75));
-                obj = EditorGUILayout.ObjectField(obj, typeof(Object), false);
-                if(preObj != obj){
-                    dependentPaths.Clear();                        
+                m_obj = EditorGUILayout.ObjectField(m_obj, typeof(Object), false);
+                if(m_preObj != m_obj) {
+                    // 清除依赖文件信息
+                    m_dependentPaths.Clear();
+                    // 关闭Commit窗口
+                    SVNTool.CloseWindow();                        
                 }
-                preObj = obj;
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("Folder：", GUILayout.Width(75));
-#if UNITY_5_3_OR_NEWER            
-            folder = EditorGUILayout.DelayedTextField(folder,  GUILayout.ExpandWidth(true));
-#else
-            folder = EditorGUILayout.TextField(folder,  GUILayout.ExpandWidth(true));
-#endif   
-            if(GUILayout.Button("Browser", GUILayout.Height(14), GUILayout.Width(80))) {
-                // 默认定位到Temp目录下
-                folder = EditorUtility.OpenFolderPanel("Folder to save dependencies", projectDir + "/Temp", "" );
-            }
+                m_preObj = m_obj;
             EditorGUILayout.EndHorizontal();
             
             // 显示提示
             EditorGUILayout.BeginHorizontal();
-            if (obj){
-                if(string.IsNullOrEmpty(folder)){
-#if UNITY_5_3_OR_NEWER            
-                    EditorGUILayout.LabelField("Select the folder to save dependencies first!", EditorStyles.centeredGreyMiniLabel);
-#else
-                    EditorGUILayout.LabelField("Select the folder to save dependencies first!");
-#endif   
-                } else {
-                    if (GUILayout.Button("Check Dependencies")){
-                        GetAllDependencies(obj);
-                    }
+            if (m_obj){
+                if (GUILayout.Button("Check Dependencies")){
+                    GetAllDependencies(m_obj);
                 }
             }
             else{
-#if UNITY_5_3_OR_NEWER            
+#if UNITY_5_3_OR_NEWER
                 EditorGUILayout.LabelField("Select an prefab first!", EditorStyles.centeredGreyMiniLabel);
 #else
                 EditorGUILayout.LabelField("Select an prefab first!");
-#endif   
+#endif
             }
             EditorGUILayout.EndHorizontal();
 
 
             EditorGUILayout.BeginHorizontal();
             // 使用ScollView显示所有资源路径
-            if(dependentPaths.Count > 0) {
+            if(m_dependentPaths.Count > 0) {
                 EditorGUILayout.BeginVertical();
                 
                 EditorGUILayout.BeginHorizontal();
                 EditorGUILayout.LabelField("Dependent resource path:");
-                if(GUILayout.Button("Copy Resources", GUILayout.Height(18), GUILayout.Width(120))) {
-                    string dialog = "";
-                    if(folder.Equals(projectDir)) {
-                        dialog = "Choose root folder of project will overwrite the original resources! Are you sure to continue?";
-                    } else {
-                        dialog = "Are you sure you want to copy all dependent resources to the selected?";
-                    }
 
-                    // 弹出对话框进行确认
-                    if(EditorUtility.DisplayDialog("Copy Resource to selected folder?", dialog, "Yes", "No")){
-                        CopyAllDependencies(folder);
+                // 全选
+                if(GUILayout.Button("ALL", GUILayout.Height(18), GUILayout.Width(50))) {
+                    foreach(string key in m_dependentPaths){
+                        m_originalPaths[key] = true;
                     }
                 }
-
-                if(GUILayout.Button("Commit", GUILayout.Height(18), GUILayout.Width(140))){
-                    SVNTool.Init(dependentPaths);
+                // 全不选
+                if(GUILayout.Button("NONE", GUILayout.Height(18), GUILayout.Width(50))) {
+                    foreach(string key in m_dependentPaths){
+                        m_originalPaths[key] = false;
+                    }
                 }
                 EditorGUILayout.EndHorizontal();
 
-                scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, true);
-                foreach(string path in dependentPaths){
-                    EditorGUILayout.SelectableLabel(path, GUILayout.Height(18));
+                m_scrollPos = EditorGUILayout.BeginScrollView(m_scrollPos, false, true);
+                foreach(string path in m_dependentPaths){
+                    m_originalPaths[path] = EditorGUILayout.ToggleLeft(path, m_originalPaths[path], GUILayout.Height(18));
                 }
                 EditorGUILayout.EndScrollView();
+
+                EditorGUILayout.BeginHorizontal();
+                if(GUILayout.Button("Copy Resources")) {
+                    string folder = EditorUtility.OpenFolderPanel("Folder to save dependencies", PROJECTDIR + "/Temp", "" );
+                    if(!string.IsNullOrEmpty(folder)){
+                        string dialog = "";
+                        if(folder.Equals(PROJECTDIR)) {
+                            dialog = "Choose the root folder of project will overwrite the original resources! Are you sure to continue?";
+                        } else {
+                            dialog = "Are you sure you want to copy all selected resources to the selected folder?\n" + folder;
+                        }
+
+                        // 弹出对话框进行确认
+                        if(EditorUtility.DisplayDialog("Copy Resource to selected folder?", dialog, "Yes", "No")){
+                            CopyAllDependencies(folder);
+                        }
+                    }
+                }
+
+                if(GUILayout.Button("Commit")){
+                    SVNTool.Init(GetSelectedPaths(false));
+                }
+                EditorGUILayout.EndHorizontal();
+
                 EditorGUILayout.EndVertical();
             }
             EditorGUILayout.EndHorizontal();
@@ -134,29 +140,33 @@ namespace Dependency{
             EditorGUILayout.EndVertical();
         }
 
-        void OnInspectorUpdate() {
-            Repaint();
+        private void OnInspectorUpdate() {
+            this.Repaint();
         }
 
-        // 获取所有引用资源的路径
-        private static void GetAllDependencies(Object prefab){
-            // 清空当前的保存的依赖资源路径
-            dependentPaths.Clear();
 
-            selectedObjs.Clear();
+        /// <summary>
+        /// 获取所有引用资源的路径
+        /// </summary>     
+        /// <param name="prefab">目标操作对象</param>  
+        private static void GetAllDependencies(Object prefab){
+            // 重置所有参数
+            m_originalPaths.Clear();
+            m_dependentPaths.Clear();
+            m_selectedObjs.Clear();
 
             string currentPath = AssetDatabase.GetAssetPath(prefab);
             Object[] roots = new Object[] { prefab };
 
             // 添加prefab所在路径
-            dependentPaths.Add(currentPath);
-            selectedObjs.Add(prefab);
+            m_dependentPaths.Add(currentPath);
+            m_selectedObjs.Add(prefab);
 
             Object[] dependentObjs = EditorUtility.CollectDependencies(roots);
             string path;
             foreach(Object dependency in dependentObjs){
                 path = AssetDatabase.GetAssetPath(dependency);
-                Debug.Log(path);
+                
                 if(path == currentPath){
                     // 如果是自身，则不添加
                     continue;
@@ -165,16 +175,32 @@ namespace Dependency{
                 // 因为EditorUtility.CollectDependencies有可能返回默认的Unity内置资源
                 // 因此需要根据返回形式来判断当前引用是否为默认资源
                 if(File.Exists(path)){
-                    dependentPaths.Add(path);
-                    selectedObjs.Add(dependency);
+                    m_dependentPaths.Add(path);
+                    m_selectedObjs.Add(dependency);
                 }
+            }
+
+            foreach (var dependentPath in m_dependentPaths) {
+                m_originalPaths.Add(dependentPath, true);
             }
         }
 
-        // 复制资源到指定文件夹
+        /// <summary>
+        /// 复制资源到指定文件夹
+        /// </summary>     
+        /// <param name="copyFolder">目标文件夹</param>  
         private static void CopyAllDependencies(string copyFolder){
-            // 重置当前未复制的文件数
-            remainCount = dependentPaths.Count;
+            // 重置需要复制的文件数
+            m_remainCount = 0;
+
+            foreach(string path in m_dependentPaths) {
+                if(m_originalPaths[path] == true){
+                    // 计算需要复制的文件数
+                    m_remainCount++;
+                }
+            }
+
+
             // 开始复制资源操作
             string sourPath, destPath;
             string destDirectory;
@@ -184,26 +210,26 @@ namespace Dependency{
                     Directory.CreateDirectory(copyFolder);
                 }
 
-                foreach(string dependentPath in dependentPaths){
-                    sourPath = projectDir + "/" + dependentPath;
-                    destPath = copyFolder + "/" + dependentPath;
+                foreach(string path in GetSelectedPaths(true)){
+                    sourPath = PROJECTDIR + "/" + path;
+                    destPath = copyFolder + "/" + path;
                     destDirectory = Path.GetDirectoryName(destPath);
                     
-                    // 创建目标资源所在的文件夹
+                    // 确保目标路径文件夹存在
                     if(!Directory.Exists(destDirectory)){
                         Directory.CreateDirectory(destDirectory);
                     }
 
                     // 采用强制覆盖的形式拷贝资源
                     File.Copy(sourPath, destPath, true);
-                    remainCount --;
+                    m_remainCount --;
 
                     // 显示进度条
-                    EditorUtility.DisplayProgressBar("Copy progress", "Remians " + remainCount + " files haven't been copyed.", 1.0f * remainCount / dependentPaths.Count);
+                    EditorUtility.DisplayProgressBar("Copy progress", "Remians " + m_remainCount + " files haven't been copyed.", 1.0f * m_remainCount / m_dependentPaths.Count);
                 }
             } catch (System.Exception e) {
                 EditorUtility.DisplayDialog("Copy faild", e.Message, "OK", "");
-                // 创建目录失败
+                // 拷贝资源失败
                 return;
             }
 
@@ -211,9 +237,28 @@ namespace Dependency{
             EditorUtility.ClearProgressBar();
         }
 
-        // 修改文件路径里面的分隔符
-        private static string ModifyPath(string path){
-            return path.Replace('\\', '/');
+
+        /// <summary>
+        /// 获得所有选中的文件路径
+        /// </summary>     
+        /// <param name="addMetaFile">是否添加文件对应的meta文件</param>      
+        private static List<string> GetSelectedPaths(bool addMetaFile = false) {
+            List<string> selectedPaths = new List<string>();
+
+            foreach(string path in m_dependentPaths) {
+                if(m_originalPaths[path] == true){
+                    // 添加源文件
+                    selectedPaths.Add(path);
+
+                    if(addMetaFile) {
+                        if(File.Exists(path + ".meta")) {
+                            selectedPaths.Add(path + ".meta");
+                        }
+                    }
+                }
+            }
+
+            return selectedPaths;
         }
     }
 }
--- Assets/Editor/SVNUtility.cs
+++ Assets/Editor/SVNUtility.cs
@@ -7,9 +7,8 @@ public class SVNUtility {
 	/// <summary>  
   	/// 运行SVN命令     
   	/// </summary>       
-  	/// <param name="arg"></param>
+  	/// <param name="arg">命令行参数</param>
 	private static void SvnCommandRun(string arg) {          
-  		// string workDirectory = Application.dataPath.Remove(Application.dataPath.LastIndexOf("/Assets", StringComparison.Ordinal)); 
 		string workDirectory = System.Environment.CurrentDirectory;
   		System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo {
 					UseShellExecute = false,			// 设置为false，process将会直接通过可执行文件来创建，而不是通过shell来创建
@@ -21,10 +20,9 @@ public class SVNUtility {
 	} 
 
 	/// <summary>        
-	/// SVN更新指定的单个文件        
-	/// 路径示例：Assets/1.png        
+	/// SVN更新指定的单个文件 
 	/// </summary>        
-	/// <param name="assetPaths"></param>        
+	/// <param name="assetPaths">文件路径，如Assets/1.png</param>       
 	public static void UpdateAtPath(string assetPath) {            
 		List<string> assetPaths = new List<string>();          
 		assetPaths.Add(assetPath);            
@@ -32,10 +30,9 @@ public class SVNUtility {
 	}
 
 	/// <summary>       
-	/// SVN更新指定的多个文件
-	/// 路径示例：Assets/1.png        
+	/// SVN更新指定的多个文件  
 	/// </summary>        
-	/// <param name="assetPaths"></param>       
+	/// <param name="assetPaths">存放多个文件路径的List，每个文件路径的格式为Assets/1.png</param>       
 	public static void UpdateAtPaths(List<string> assetPaths) {            
 		// 没有需要update的资源，不执行任何操作
 		if (assetPaths.Count == 0) {                
@@ -58,15 +55,42 @@ public class SVNUtility {
 	}
 
 	/// <summary>        
-	/// SVN提交指定的路径     
-	/// 路径示例：Assets/1.png    
+	/// SVN添加指定的多个文件到版本控制
 	/// </summary>     
-	/// <param name="assetPaths"></param>      
+	/// <param name="assetPaths">存放多个文件路径的List，每个文件路径的格式为Assets/1.png</param>
+	private static void AddPaths(List<string> assetPaths) {            
+		// 没有需要commit的资源，不执行任何操作
+		if (assetPaths.Count == 0) {              
+			return;        
+		}
+		string arg = "/command:add /closeonend:0 /path:\"";      
+		for (int i = 0; i < assetPaths.Count; i++) {             
+			var assetPath = assetPaths[i];             
+			if (i != 0) {                  
+				arg += "*";
+			}             
+			arg += assetPath;
+		}         
+		arg += "\"";        
+
+		// 执行指令
+		SvnCommandRun(arg);
+	}
+
+	/// <summary>
+	/// SVN提交指定的多个文件
+	/// </summary>     
+	/// <param name="assetPaths">存放多个文件路径的List，每个文件路径的格式为Assets/1.png</param>      
+	/// <param name="logmsg">commit时提交的log信息</param>      
 	public static void CommitAtPaths(List<string> assetPaths, string logmsg = null) {            
 		// 没有需要commit的资源，不执行任何操作
 		if (assetPaths.Count == 0) {              
 			return;        
 		}          
+
+		// commit之前需要先add
+		AddPaths(assetPaths);
+
 		string arg = "/command:commit /closeonend:0 /path:\"";      
 		for (int i = 0; i < assetPaths.Count; i++) {             
 			var assetPath = assetPaths[i];             
@@ -76,13 +100,49 @@ public class SVNUtility {
 			arg += assetPath;         
 		}         
 		arg += "\"";
-		
+
 		// 附加log信息
 		if (!string.IsNullOrEmpty(logmsg)) {              
 			arg += " /logmsg:\"" + logmsg + "\"";   
-		}            
+		} 
 
 		// 执行指令
-		SvnCommandRun(arg);      
+		SvnCommandRun(arg);
+		Debug.Log("CommitAtPaths");
+	}
+
+	public static void CommitFolderAndFile(List<string> filePaths, List<string> folderPaths, string logmsg = null) {
+		// 没有需要commit的资源，不执行任何操作
+		if (filePaths.Count == 0) {              
+			return;
+		}
+
+		// 先Add文件，再Add文件夹，避免重复Add
+		AddPaths(filePaths);
+		AddPaths(folderPaths);
+
+		// 按照Add的顺序进行Commit
+		string arg = "/command:commit /closeonend:0 /path:\"";
+		for(int i = 0; i < filePaths.Count; i++) {
+			var path = filePaths[i];             
+			if (i != 0) {
+				arg += "*";          
+			}             
+			arg += path;
+		}
+		for (int i = 0; i < folderPaths.Count; i++) {             
+			var path = folderPaths[i];             
+			arg += "*" + path;         
+		}     
+		arg += "\"";
+
+		// 附加log信息
+		if (!string.IsNullOrEmpty(logmsg)) {              
+			arg += " /logmsg:\"" + logmsg + "\"";   
+		} 
+
+		// 执行指令
+		SvnCommandRun(arg);
+		Debug.Log("CommitFolderAndFile");
 	}
 }
